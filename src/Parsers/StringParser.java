2b060538760f55409ed1fe60677a34e32ea7aa2b
andrea-genovese <andreagenovese01@gmail.com>
2022-06-08 23:21:37 +0200

    unescaping implemented

--- src/Parsers/StringParser.java
+++ src/Parsers/StringParser.java
@@ -1,21 +1,52 @@
 package Parsers;
 
+import java.util.regex.Pattern;
+
 public class StringParser {
+    private static final Pattern INVALID = Pattern.compile("(?<!\\\\)\\\\(?![\"\\\\/bfnrt]|u[\\da-f]{4})",
+            Pattern.MULTILINE);
+    private static final Pattern QUOT = Pattern.compile("\\\\\"", Pattern.MULTILINE);
+    private static final Pattern B_SLASH = Pattern.compile("\\\\\\\\", Pattern.MULTILINE);
+    private static final Pattern BACKSPACE = Pattern.compile("\\\\b", Pattern.MULTILINE);
+    private static final Pattern FORMFEED = Pattern.compile("\\\\f", Pattern.MULTILINE);
+    private static final Pattern LF = Pattern.compile("\\\\n", Pattern.MULTILINE);
+    private static final Pattern CR = Pattern.compile("\\\\r", Pattern.MULTILINE);
+    private static final Pattern TAB = Pattern.compile("\\\\t", Pattern.MULTILINE);
+    private static final Pattern CODEPOINT = Pattern.compile("\\\\u([\\da-f]{4})", Pattern.MULTILINE);
+
     private static String createString(char[] arr, int start, int end) {
-        char[] subarray = new char[end-start];
+        char[] subarray = new char[end - start];
         for (int i = 0; i < subarray.length; i++) {
-            subarray[i] = arr[start+i];
+            subarray[i] = arr[start + i];
         }
-        return new String(subarray);
+        return unescape(new String(subarray));
     }
+
+    private static String unescape(String string) {
+        if (INVALID.matcher(string).find()) {
+            throw new InvalidJSONException("Invalid escape");
+        }
+        string = QUOT.matcher(string).replaceAll("\"");
+        string = B_SLASH.matcher(string).replaceAll("\\");
+        string = BACKSPACE.matcher(string).replaceAll("\b");
+        string = FORMFEED.matcher(string).replaceAll("\f");
+        string = LF.matcher(string).replaceAll("\n");
+        string = CR.matcher(string).replaceAll("\r");
+        string = TAB.matcher(string).replaceAll("\t");
+        return CODEPOINT.matcher(string).replaceAll(match -> 
+            Character.toString(Integer.parseInt(match.group(1), 16))
+        );
+    }
+
     public static ParseResult parse(char[] json, int index) {
-        if(index >= json.length - 1 || json[index] != '"') { //controlla se c'è spazio per una stringa e se il primo carattere è '"'
+        if (index >= json.length - 1 || json[index] != '"') { // controlla se c'è spazio per una stringa e se il primo
+                                                              // carattere è '"'
             return null;
         }
-        for(int i = index+1; i < json.length; i++) {
-            if(json[i] == '"' && json[i-1] != '\\' ) {
+        for (int i = index + 1; i < json.length; i++) {
+            if (json[i] == '"' && json[i - 1] != '\\') {
 
-                return new ParseResult(createString(json, index+1, i), i+1);
+                return new ParseResult(createString(json, index + 1, i), i + 1);
             }
         }
         return null;
--- src/test/main.java
+++ src/test/main.java
@@ -1,24 +1,11 @@
 package test;
 
-import main.JSON;
-import main.JSONArray;
-import main.JSONObject;
+import Parsers.StringParser;
 public class Main {
     //private static final String o1 = "{\"abc\":1}";
-    private static final String o2 = " { \"abc\" : [ 1 , 2 , 3 ] , \"def\" : { } , \"ghi\" : { \"a\" : \"A\" , \"b\" : [ { } , { } ] } } ";
+    private static final String o2 = " { \"\\abc\" : [ 1 , 2 , 3 ] , \"def\" : { } , \"ghi\" : { \"a\" : \"A\" , \"b\" : [ { } , { } ] } } ";
     public static void main(String[] args) {
-
-        try {
-            Object o = JSON.parse(o2);
-            if(o instanceof JSONArray) {
-                JSONObject m = (JSONObject) o;
-                System.out.println(
-                    m
-                );
-            }
-            
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        
+        System.out.println(Character.toString('❤'));
     }
 }
